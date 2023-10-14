d1fbe5d1b4fbf918a35b0493c92fc59e4f58edd8
hac4us06 <hac4usanime@gmail.com>
2023-10-14 19:56:41 +0200

    chime: derpfest A14 bringup
    Signed-off-by: hac4us06 <hac4usanime@gmail.com>

--- derp_citrus.mk
+++ derp_citrus.mk
@@ -4,20 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from lime device
-$(call inherit-product, device/xiaomi/chime/device-lime.mk)
+# Inherit from citrus device
+$(call inherit-product, device/xiaomi/chime/device-citrus.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/halcyon/config/common.mk)
-TARGET_USES_AOSP_RECOVERY := true
+$(call inherit-product, vendor/derp/config/common.mk)
+USE_LEGACY_BOOTANIMATION := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-
+DERP_BUILDTYPE := Official
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := halcyon_lime
-PRODUCT_DEVICE := lime
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 9T
+PRODUCT_NAME := derp_citrus
+PRODUCT_DEVICE := citrus
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO M3
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
--- derp_lime.mk
+++ derp_lime.mk
@@ -4,20 +4,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from citrus device
-$(call inherit-product, device/xiaomi/chime/device-citrus.mk)
+# Inherit from lime device
+$(call inherit-product, device/xiaomi/chime/device-lime.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/halcyon/config/common.mk)
-TARGET_USES_AOSP_RECOVERY := true
+$(call inherit-product, vendor/derp/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+USE_LEGACY_BOOTANIMATION := true
+DERP_BUILDTYPE := Official
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := halcyon_citrus
-PRODUCT_DEVICE := citrus
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO M3
+PRODUCT_NAME := derp_lime
+PRODUCT_DEVICE := lime
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 9T
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
