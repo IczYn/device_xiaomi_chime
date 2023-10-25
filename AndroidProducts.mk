210cc3b93ad3e40929816a4ee47da8d3b2cb8a23
IczYn <iczyn.iczyn@gmail.com>
2023-10-25 15:51:20 +0200

    chime: clean trees
    Signed-off-by: IczYn <iczyn.iczyn@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,8 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := \
-<<<<<<< HEAD
     $(LOCAL_DIR)/evolution_citrus.mk \
     $(LOCAL_DIR)/evolution_lime.mk
 
@@ -16,15 +14,3 @@ COMMON_LUNCH_CHOICES := \
     evolution_lime-user \
     evolution_lime-userdebug \
     evolution_lime-eng
-=======
-    $(LOCAL_DIR)/derp_citrus.mk \
-    $(LOCAL_DIR)/derp_lime.mk
-
-COMMON_LUNCH_CHOICES := \
-    derp_citrus-user \
-    derp_citrus-userdebug \
-    derp_citrus-eng \
-    derp_lime-user \
-    derp_lime-userdebug \
-    derp_lime-eng
->>>>>>> parent of cb260c9 (rootdir: Add auto logger script)
