38cb5209449022ac4f8d9e801de16c562780bf2d
IczYn <iczyn.iczyn@gmail.com>
2023-10-26 11:34:12 +0200

    chime:bringup
    Signed-off-by: IczYn <iczyn.iczyn@gmail.com>

--- chime.mk
+++ chime.mk
@@ -323,8 +323,9 @@ PRODUCT_PACKAGES += \
 
 # Perf
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2 \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.0.vendor \
+    vendor.qti.hardware.perf@2.1.vendor \
+    vendor.qti.hardware.perf@2.2.vendor \
 
 # Permissions
 PRODUCT_COPY_FILES += \
