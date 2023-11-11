354c441b82c27e7a17800836fa26fb9cb2dda988
IczYn <iczyn.iczyn@gmail.com>
2023-11-11 12:15:23 +0100

    chime: derp14 bringup
    Signed-off-by: IczYn <iczyn.iczyn@gmail.com>

--- derp_citrus.mk
+++ derp_citrus.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, device/xiaomi/chime/device-citrus.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/derp/config/common.mk)
 USE_LEGACY_BOOTANIMATION := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 DERP_BUILDTYPE := Official
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_citrus
+PRODUCT_NAME := derp_citrus
 PRODUCT_DEVICE := citrus
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO M3
